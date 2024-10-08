0d3f46c239fee685e9518146b54c196c77036c80
mlobf <marcoslemeborbafilho@gmail.com>
2024-10-07 21:43:02 -0300

    teste dois

--- sample_project/main/utils/dataclasses/city/city.py
+++ sample_project/main/utils/dataclasses/city/city.py
@@ -10,8 +10,53 @@ class City:
 
     def __post_init__(self):
         if not isinstance(self.id, (str, type(None))):
-            raise TypeError(f"Expected 'id' to be 'Optional[str]', got {type(self.id).__name__}")
+            raise TypeError(
+                f"Expected 'id' to be 'Optional[str]', got {
+                    type(self.id).__name__}"
+            )
         if not isinstance(self.city_name, str):
-            raise TypeError(f"Expected 'city_name' to be 'str', got {type(self.city_name).__name__}")
+            raise TypeError(
+                f"Expected 'city_name' to be 'str', got {
+                    type(self.city_name).__name__}"
+            )
         if not isinstance(self.short_name, str):
-            raise TypeError(f"Expected 'short_name' to be 'str', got {type(self.short_name).__name__}")
+            raise TypeError(
+                f"Expected 'short_name' to be 'str', got {
+                    type(self.short_name).__name__}"
+            )
+
+
+@dataclass
+class Cassino:
+    id: Optional[str] = None
+    cassino_name: str
+    short_name: str
+    address: str
+    manager: str
+
+    def __post_init__(self):
+        if not isinstance(self.id, (str, type(None))):
+            raise TypeError(
+                f"Expected 'id' to be 'Optional[str]', got {
+                    type(self.id).__name__}"
+            )
+        if not isinstance(self.cassino_name, str):
+            raise TypeError(
+                f"Expected 'city_name' to be 'str', got {
+                    type(self.cassino_name).__name__}"
+            )
+        if not isinstance(self.short_name, str):
+            raise TypeError(
+                f"Expected 'short_name' to be 'str', got {
+                    type(self.short_name).__name__}"
+            )
+        if not isinstance(self.address, str):
+            raise TypeError(
+                f"Expected 'short_name' to be 'str', got {
+                    type(self.address).__name__}"
+            )
+        if not isinstance(self.manager, str):
+            raise TypeError(
+                f"Expected 'short_name' to be 'str', got {
+                    type(self.manager).__name__}"
+            )
